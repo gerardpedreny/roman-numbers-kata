819b4e03148799ee43d77af0f4153ce2c515f809
Gerard Pedreny <gerard.pedreny@scmspain.com>
2015-07-05 21:03:24 +0200

    Refactor Tests - Merge all direct translation numbers tests

--- src/test/java/RomanNumbersTest.java
+++ src/test/java/RomanNumbersTest.java
@@ -14,65 +14,15 @@ public class RomanNumbersTest {
     }
 
     @Test
-    public void givenOneWhenArabicToRomanCalledShouldReturnI() {
+    public void givenDirectTranslationNumbersWhenArabicToRomanCalledShouldReturnCorrectRomanNumbers() {
         RomanNumbers romanNumbers = new RomanNumbers();
 
-        String translation = romanNumbers.arabicToRoman(1);
-
-        assertEquals("I", translation);
-    }
-
-    @Test
-    public void givenFiveWhenArabicToRomanCalledShouldReturnV() {
-        RomanNumbers romanNumbers = new RomanNumbers();
-
-        String translation = romanNumbers.arabicToRoman(5);
-
-        assertEquals("V", translation);
-    }
-
-    @Test
-    public void givenTenWhenArabicToRomanCalledShouldReturnX() {
-        RomanNumbers romanNumbers = new RomanNumbers();
-
-        String translation = romanNumbers.arabicToRoman(10);
-
-        assertEquals("X", translation);
-    }
-
-    @Test
-    public void givenFiftyWhenArabicToRomanCalledShouldReturnL() {
-        RomanNumbers romanNumbers = new RomanNumbers();
-
-        String translation = romanNumbers.arabicToRoman(50);
-
-        assertEquals("L", translation);
-    }
-
-    @Test
-    public void givenAHundredWhenArabicToRomanCalledShouldReturnC() {
-        RomanNumbers romanNumbers = new RomanNumbers();
-
-        String translation = romanNumbers.arabicToRoman(100);
-
-        assertEquals("C", translation);
-    }
-
-    @Test
-    public void givenFiveHundredWhenArabicToRomanCalledShouldReturnD() {
-        RomanNumbers romanNumbers = new RomanNumbers();
-
-        String translation = romanNumbers.arabicToRoman(500);
-
-        assertEquals("D", translation);
-    }
-
-    @Test
-    public void givenAThousandWhenArabicToRomanCalledShouldReturnM() {
-        RomanNumbers romanNumbers = new RomanNumbers();
-
-        String translation = romanNumbers.arabicToRoman(1000);
-
-        assertEquals("M", translation);
+        assertEquals("I", romanNumbers.arabicToRoman(1));
+        assertEquals("V", romanNumbers.arabicToRoman(5));
+        assertEquals("X", romanNumbers.arabicToRoman(10));
+        assertEquals("L", romanNumbers.arabicToRoman(50));
+        assertEquals("C", romanNumbers.arabicToRoman(100));
+        assertEquals("D", romanNumbers.arabicToRoman(500));
+        assertEquals("M", romanNumbers.arabicToRoman(1000));
     }
 }
